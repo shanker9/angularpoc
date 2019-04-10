b7aba561c3d595754aee18d1cc285868540826ea
Shanker <ssnani9@gmail.com>
2019-04-10 11:34:19 -0400

    added strategy title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string = 'SampleAngularApp';
-  names : string[] = ["Water_Ice_cream","Settings_Comics","Crab_Floppy_Disk","Hnads_Dislike","Soap_Websites","Soda_Soda","Settings_Ice_cream_cone","Flowers_Ring","Kitty_Urine","Settings_Boat","Prints_Puppy","Ice_cream_Whale","Male_Android","Soda_Urine","Monster_Trees","Rollers_Comics","Plus_Shelf","Shoe_Whale","Towel_Ice_cream","Puppy_Fence","System_Poop","Solar_Dislike","Post_office_Trees","Book_Shower","Toolbox_Robot","Monster_System","Website_Mail","Post_office_Whale","Prints_Cat","Leash_Kitty","Urine_Video_games","Shoes_Towel","Flowers_Floppy_Disk","System_Soda","Comics_Crab","Toolbox_Ring","Website_Cat","Toolbox_Mail","Ice_cream_Rollers","Urine_Book","Plants_Male","Bird_Plants","Body_Comics","Video_games_Bird","Shelf_Monster","Nuclear_Mail","Trees_YouTube","Fusion_Shoe","Solar_Laptop","Hnads_Body"];
+  names: string[] = ["Water_Ice_cream", "Settings_Comics", "Crab_Floppy_Disk", "Hnads_Dislike", "Soap_Websites", "Soda_Soda", "Settings_Ice_cream_cone", "Flowers_Ring", "Kitty_Urine", "Settings_Boat", "Prints_Puppy", "Ice_cream_Whale", "Male_Android", "Soda_Urine", "Monster_Trees", "Rollers_Comics", "Plus_Shelf", "Shoe_Whale", "Towel_Ice_cream", "Puppy_Fence", "System_Poop", "Solar_Dislike", "Post_office_Trees", "Book_Shower", "Toolbox_Robot", "Monster_System", "Website_Mail", "Post_office_Whale", "Prints_Cat", "Leash_Kitty", "Urine_Video_games", "Shoes_Towel", "Flowers_Floppy_Disk", "System_Soda", "Comics_Crab", "Toolbox_Ring", "Website_Cat", "Toolbox_Mail", "Ice_cream_Rollers", "Urine_Book", "Plants_Male", "Bird_Plants", "Body_Comics", "Video_games_Bird", "Shelf_Monster", "Nuclear_Mail", "Trees_YouTube", "Fusion_Shoe", "Solar_Laptop", "Hnads_Body"];
 
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  onScroll(e) {
+    let elem = document.getElementById('structure_header');
+    elem.scrollTop = document.getElementById('structure_body').scrollTop;
   }
 }
--- src/app/strategy/strategy.component.ts
+++ src/app/strategy/strategy.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class StrategyComponent implements OnInit {
 
   @Input() name: string;
+  legs : Array<string> = [];
 
   constructor() { }
 
   ngOnInit() {
+    for(let i=1; i<10;i++){
+      this.legs.push(`leg${i}`);
+    }
   }
 
 }
